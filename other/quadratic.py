cf89767aa187ec6feace43c0b9a8debc9b502145
KalloyanTs <bassline1911@mail.com>
2024-03-20 20:18:04 +0200

    New

---
+++ other/quadratic.py
@@ -0,0 +1,20 @@
+import math
+
+def quad_equasion(a, b, c):
+    d = (b*b - (4*a*c))
+    if d < 0:
+        print("No real roots")
+    elif d == 0:
+        print("x1 and x2 are equal to:", end="")
+        x = (-b)/(2*a)
+        return x
+    elif d > 0:
+         x1 = (-b + math.sqrt(d))/(2*a)
+         x2 = (-b - math.sqrt(d))/(2*a)
+         return x1, x2 
+
+givea = int(input("Type the a coeficient:"))
+giveb = int(input("Type the b coeficient:"))
+givec = int(input("Type the c coeficient:"))
+
+print(quad_equasion(givea, giveb, givec))
