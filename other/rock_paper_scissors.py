2138a78b9148bf9968f32fef78f1fcdd60152358
KalloyanTs <bassline1911@mail.com>
2024-04-14 22:29:58 +0300

    ask to continue or not

--- other/rock_paper_scissors.py
+++ other/rock_paper_scissors.py
@@ -1,40 +1,43 @@
 import random
-print("Hi, this is a rock, paper and scissors game!\nPlease give your input below\nFor short you can use: \nr for rock\np for paper\ns for scissors\n ")
 
-game_options = {
-    's' : 'scissors',
-    'r' : 'rock', 
-    'p' : 'paper',
-    'scissors' : 'scissors',
-    'rock' : 'rock',
-    'paper' : 'paper'
-}
+def game():
+    print("Hi, this is a rock, paper and scissors game!\nPlease give your input below\nFor short you can use: \nr for rock\np for paper\ns for scissors\n ")
+    game_options = {
+        's' : 'scissors',
+        'r' : 'rock', 
+        'p' : 'paper',
+        'scissors' : 'scissors',
+        'rock' : 'rock',
+        'paper' : 'paper'
+        }
+    def user_choice():
+        while True:
+            user_input = str(input("Input: "))
+            choice = game_options.get(user_input)
+            if choice:
+                return choice
+            else:
+                print(f"Incorrect choice, please enter a correct choice(r, s or p).")
 
-def user_choice():
-    while True:
-        user_input = str(input("Input: "))
-        choice = game_options.get(user_input)
-        #print(choice)
-        if choice:
-            return choice
+    def comp_choice():
+        return game_options.get(random.choice([x for x in game_options]))
+    
+    def winner(user, computer):
+        if user == computer:
+            return "\nIt's a Tie!"
+        elif (user == 'scissors' and computer == 'paper') or (user == 'rock' and computer == 'scissors') or (user == 'paper' and computer == 'rock'):
+            return "\nCongratulations, you Win!"
         else:
-            print(f"Incorrect choice, please enter a correct choice(r, s or p).")
-
-def comp_choice():
-    return game_options.get(random.choice([x for x in game_options]))
-
-def winner(user, computer):
-    if user == computer:
-        return "It's a Tie!"
-    elif (user == 'scissors' and computer == 'paper') or (user == 'rock' and computer == 'scissors') or (user == 'paper' and computer == 'rock'):
-        return "Congratulations, you Win!"
-    else:
-        return "Sorry, you lose!"
-
-def main():
+            return "\nSorry, you lose!"
+    
     user = user_choice()
     computer = comp_choice()
-    print(f"Your choice is: {user} \n The computer rolled: {computer}! \n {winner(user, computer)}")
-
-main()
-
+    print(f"Your choice is: {user} \nThe computer rolled: {computer}! \n {winner(user, computer)}")
+    
+    end_input = input("\nWould you like to play again?(y): ")
+    if end_input in ['yes', 'y']:
+        game()
+    else: 
+        print("\nTerminating")
+        exit()
+game()
