446ed04594e95aa14c384e37aebd8d9d67535eaf
KalloyanTs <bassline1911@mail.com>
2024-07-26 22:52:40 +0300

    MNK_python

--- MNK_python/leapyear.py
+++ MNK_python/leapyear.py
@@ -1,10 +1,5 @@
 yearinput = int(input("Enter year: "))
-div_by_4 = yearinput % 4
-div_by_100 = yearinput % 100
-div_by_400 = yearinput % 400
-
-
-if div_by_4 == 0 and (div_by_400 == 0 or div_by_100 != 0):
+if yearinput % 4 == 0 and (yearinput % 400 == 0 or yearinput % 100 != 0):
     print(f"The year {yearinput} is a leap year!")
 else:
     print(f"This year {yearinput} is NOT a leap year!")
